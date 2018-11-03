121ba974af639499260fca2680e47c39947c07a0
Nicholas Bernhard <nicholas@NDHFilms.com>
2018-11-02 23:14:15 -0600

    adding a 20

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@
 var LocalStrategy = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 var TwitterStrategy = require('passport-twitter').Strategy;
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // load up the user model
 var User = require('../models/Users');
